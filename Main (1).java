5ade9e202bf43cf33d8d66b992aca55f89420f96
Suruthi V <velusamysuruthi14@gmail.com>
2025-07-08 21:29:53 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		int weight=0;
+		int onion=2;
+	    weight=onion;
+		
+		System.out.print("The weight is "+weight);
+	}
+}
